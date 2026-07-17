712ec93f0c3a8a4d37bc060db72e3efb559d7c61
smithdns04-star <smithdns04@gmail.com>
2026-07-17 05:51:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Mobile navigation responsive toggle
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-if (mobileMenu && navLinks) {
-    mobileMenu.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-}
